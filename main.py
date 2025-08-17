cf9cd7414a03d22f9ebdcb2c74f74b423730f736
runtimeanxiety <winkie.storm@gmail.com>
2025-08-17 13:13:49 +0800

    added count_words() to main.py

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
-def main():
-    return get_book_text("books/frankenstein.txt")
+def count_words():
+    num_words = 0
+    file_contents = get_book_text("books/frankenstein.txt")
+    words = file_contents.split()
+    for word in words:
+        num_words += 1
+    return num_words
 
-print(main())
+def main():
+    print(f"{count_words()} words found in the document")
+
+main()
