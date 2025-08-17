63d7ce09bbcb3cc0de739d07831bdfb7d2c6f3eb
runtimeanxiety <winkie.storm@gmail.com>
2025-08-17 13:21:20 +0800

    refactored main.py to stats.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-    return file_contents
+from stats import get_num_words
 
-def count_words():
-    num_words = 0
-    file_contents = get_book_text("books/frankenstein.txt")
-    words = file_contents.split()
-    for word in words:
-        num_words += 1
-    return num_words
 
 def main():
-    print(f"{count_words()} words found in the document")
+    print(f"{get_num_words()} words found in the document")
+
 
 main()
---
+++ stats.py
@@ -0,0 +1,13 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def get_num_words():
+    num_words = 0
+    file_contents = get_book_text("books/frankenstein.txt")
+    words = file_contents.split()
+    for word in words:
+        num_words += 1
+    return num_words
